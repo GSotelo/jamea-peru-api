28a332eda8dc5ecc5ca04a8f582a6cc2a3f21b33
gsotelo <gsotelo.se@gmail.com>
2024-08-01 12:29:41 +0200

    Defining DTOs
    DTOs for soup service.

--- src/soups/soups.controller.ts
+++ src/soups/soups.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SoupsService } from './soups.service';
 
 @Controller('soups')
@@ -8,5 +8,10 @@ export class SoupsController {
   @Get()
   getSoups() {
     return this.soupsService.findAll();
+  }
+
+  @Get(':id')
+  getSoupById(@Param('id') id: string) {
+    return this.soupsService.findById(id);
   }
 }
--- src/soups/soups.service.ts
+++ src/soups/soups.service.ts
@@ -1,17 +1,47 @@
 import { Injectable } from '@nestjs/common';
 
-type FindSoups = { soups: string[] };
+export type SoupId =
+  | 'sopa-wantan'
+  | 'chilcano-de-pescado'
+  | 'menestron-de-carne'
+  | 'caldo-de-gallina';
+
+export type SoupName =
+  | 'Sopa Wantan'
+  | 'Chilcano de Pescado'
+  | 'Menestrón de Carne'
+  | 'Caldo de Gallina';
+
+export type SoupOverview = {
+  id: SoupId;
+  name: SoupName;
+};
+
+export type SoupRecipe = {
+  ingredients: string[];
+} & SoupOverview;
+
+export type FindSoup = (id: string) => SoupRecipe;
+
+export type FindSoups = () => { soups: SoupOverview[] };
 
 @Injectable()
 export class SoupsService {
-  findAll(): FindSoups {
+  findAll: FindSoups = () => {
+    const soups: SoupOverview[] = [
+      { id: 'sopa-wantan', name: 'Sopa Wantan' },
+      { id: 'chilcano-de-pescado', name: 'Chilcano de Pescado' },
+      { id: 'menestron-de-carne', name: 'Menestrón de Carne' },
+      { id: 'caldo-de-gallina', name: 'Caldo de Gallina' },
+    ];
+    return { soups };
+  };
+
+  findById: FindSoup = (id: SoupId) => {
     return {
-      soups: [
-        'Sopa Wantan',
-        'Chilcano de Pescado',
-        'Menestrón de Carne',
-        'Caldo de Gallina',
-      ],
+      id,
+      name: 'Sopa Wantan',
+      ingredients: ['pollo', 'verduras', 'fideos', 'huevo'],
     };
-  }
+  };
 }
