a6231d9c5db403bb8f8acdfd6b968256c0799a33
gsotelo <gsotelo.se@gmail.com>
2024-08-08 20:56:53 +0200

    Security infrastructure
    Add helmet.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({ type: VersioningType.URI });
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
