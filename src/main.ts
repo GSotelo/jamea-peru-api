09fa3e71529c09d1c6678c620809c229c630ecd8
gsotelo <gsotelo.se@gmail.com>
2024-08-09 22:13:33 +0200

    Performance optimization
    Add 'compression'.

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({ type: VersioningType.URI });
-  app.use(helmet());
   app.enableCors();
+  app.use(helmet());
+  app.use(compression());
   await app.listen(3000);
 }
 bootstrap();
